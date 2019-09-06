e1c6ff41d755c3e3fcc63aa7d266a0568900bde5
K <721662@cognizant.com>
2019-09-06 12:08:15 +0530

    Loading Spinner and Error check

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '../../../node_modules/@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -8,19 +9,45 @@ import { NgForm } from '../../../node_modules/@angular/forms';
 })
 export class AuthComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
   isLoginMode = true;
+  isLoading = false;
+  error: string = null;
 
   onSwitchMode() {
     this.isLoginMode = ! this.isLoginMode;
   }
 
   onSubmit(form: NgForm) {
-    console.log(form.value);
+    
+    if(!form.valid) {
+      return;
+    }
+
+    this.isLoading = true;
+    if(this.isLoginMode) {
+      //..
+    } else {
+      const email = form.value.email;
+      const password = form.value.password;
+  
+      this.authService.signUp(email, password).subscribe(
+        resData => {
+          console.log(resData);
+          this.isLoading = false;
+        },
+        error => {
+          console.log(error);
+          this.error = "An error occurred!";
+          this.isLoading = false;
+        }
+      );
+  
+    }
     form.reset();
   }
 
